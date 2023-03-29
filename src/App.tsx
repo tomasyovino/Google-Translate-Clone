e07501e35cd9afa6e0677e7a461753c3937e3e22
tomasyovino <tomasyovino@gmail.com>
2023-03-29 17:05:41 -0300

    Display initial structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Google Translate</h1>
+    </div>
+  )
+}
+
+export default App
